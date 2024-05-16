f980a8ae52539cf35f9b5a5d2bc7ef18b6336fb3
sid_Noori <127247235+No-o-ri@users.noreply.github.com>
2024-05-16 22:05:03 +0000

    Changed it to a compound interest calculator

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,4 +1,23 @@
 from utils import process_workbook_data
+compound = None
+interest = None
+time_duration = None
+compound_num = None
+try:
+    interest = float(input("Enter the interest rate:"))
+    compound = str(input("Is this compounded monthly, annually, or weekly,:"))
+    time_duration = int(input("How long are you letting this build up:"))
+except ValueError or TypeError:
+    print("Invalid Values Entered")
 
 
-process_workbook_data("PyAuto.xlsx")
+compound_dictionary = {
+    "monthly": 12,
+    "annually": 1,
+    "weekly": 52
+}
+if compound is not None:
+    compound_num = compound_dictionary.get(compound.lower(), 'Not valid duration')
+
+
+process_workbook_data("PyAuto.xlsx", interest, compound_num, time_duration)
--- pythonProject/utils.py
+++ pythonProject/utils.py
@@ -1,21 +1,28 @@
+
 import openpyxl as xl
-from openpyxl.chart import BarChart, Reference
+from openpyxl.chart import LineChart, Reference
 
 
-def process_workbook_data(file_name):
+def process_workbook_data(file_name, interest, compound, time_duration):
     wb = xl.load_workbook(file_name)
     sheet = wb['Sheet1']
     for row in range(2, sheet.max_row+1):
         cell = sheet.cell(row, 3)
-        discount_price = cell.value * 0.9
-        discount_price_cell = sheet.cell(row, 4)
-        discount_price_cell.value = discount_price
+        interest_price = 0.0
+        if cell.value is None:
+            continue  # Skip empty cells
+        else:
+            interest_price = cell.value * ((1 + interest / compound) ** (compound * time_duration))
+        # interest_price = cell.value * pow((1-interest/compound), compound * time_duration)
+        interest_cell = sheet.cell(row, 4)
+        interest_cell.value = interest_price
 
     values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
-    chart = BarChart()
+    chart = LineChart()
+    chart.title = 'Interest Rates At Different Principal Prices'
+    chart.style = 8
+    chart.y_axis.title = "Price"
+    chart.x_axis.title = "Time"
     chart.add_data(values)
     sheet.add_chart(chart, 'e2')
-
     wb.save(file_name)
-
-
